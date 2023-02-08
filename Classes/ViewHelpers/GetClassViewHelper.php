c36b2ac85360624e68fc05ac535851efd509f9d5
Benjamin Beck <benjamin@Benjamins-MacBook-Pro.local>
2023-02-08 15:57:31 +0100

    remove namespaces

--- Classes/ViewHelpers/GetClassViewHelper.php
+++ Classes/ViewHelpers/GetClassViewHelper.php
@@ -3,18 +3,8 @@ declare(strict_types=1);
 
 namespace BDM\BdmVhs\ViewHelpers;
 
-use BDM\BdmFegm\Domain\Repository\FrontendUserGroupRepository;
-use TYPO3\CMS\Core\Database\Connection;
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Resource\ResourceFactory;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Service\ExtensionService;
 use Closure;
-use TYPO3\CMS\Extbase\Service\ImageService;
-use TYPO3\CMS\Frontend\Resource\FileCollector;
-use TYPO3\CMS\FrontendLogin\Service\UserService;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
-use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
 
 /**
  * UriViewHelper
